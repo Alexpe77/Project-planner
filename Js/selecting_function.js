fc5f646be4d8593bd6866e922434397decc80b32
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-05-05 11:47:12 +0200

    adding select options

--- Js/selecting_function.js
+++ Js/selecting_function.js
@@ -1,15 +1,30 @@
 export function selecting() {
-  const to_do = document.querySelector(".to-do-list");
-  const doing = document.querySelector(".doing");
-  const done = document.querySelector(".done");
+  const to_do = document.getElementById("to-do-list");
+  const doing = document.getElementById("doing");
+  const done = document.getElementById("done");
   const select = document.querySelector(".to-do-selection");
 
   if (select.value == "to-do") {
+    done.style.display = "none";
+    to_do.style.display = "block";
+    doing.style.display = "none";
     console.log("to-do");
+    return;
   } else if (select.value == "doing") {
+    done.style.display = "none";
+    to_do.style.display = "none";
+    doing.style.display = "block";
     console.log("doing");
+    return;
   } else if (select.value == "done") {
+    done.style.display = "block";
+    to_do.style.display = "none";
+    doing.style.display = "none";
     console.log("done");
+    return;
   }
+  done.style.display = "block";
+  to_do.style.display = "block";
+  doing.style.display = "block";
   console.log("all");
 }
